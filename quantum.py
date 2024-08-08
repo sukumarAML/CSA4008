71e01d5477d8e2cc2db6308ab2d7efe2be844aa3
SukumarAML <sukuraj2013@gmail.com>
2024-08-08 09:32:19 +0530

    hai

--- quantum.py
+++ quantum.py
@@ -10,3 +10,16 @@ r = np.random.rand(4, 4)
 print(Qobj(r))
 print(basis(5,3))
 print(coherent(5,0.5-0.5j))
+
+print(destroy(4))
+print(sigmaz())
+print(jmat(5/2.0,'+'))
+
+q=destroy(4)
+q.dims
+q.shape
+q.type
+q.isherm
+q.data
+
+
